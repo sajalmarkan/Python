14fc962d7e17181d2bffc2183104f195113c6343
sajalmarkan <127577621+sajalmarkan@users.noreply.github.com>
2024-07-03 18:50:35 +0530

    Secret Code

---
+++ SecretCode.py
@@ -0,0 +1,26 @@
+st= input("Enter message")
+words = st.split(" ")
+
+coding=true
+if(coding):
+    nwords =[]
+    for word in words:
+        if(len(word)>=3):
+            r1 ='sdk'
+            r2 = 'bhj'
+            stnew = r1 + word[1:] + word[0] +r2
+            nwords.append(stnew)
+        else:
+            nwords.append(stnew)
+    print(" ".join(nwords))
+
+else: 
+    nwords =[]
+    for word in words:
+        if(len(word)>=3):
+            stnew = word[3:-3]
+            stnew =stnew[-1] + stnew[:-1]
+            nwords.append(stnew)
+        else:
+            nwords.append(word[:-1])
+    print(" ".join(nwords))
