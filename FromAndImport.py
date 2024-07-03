f8efb6236e4989828f38c58882188a4e8f81ff58
sajalmarkan <127577621+sajalmarkan@users.noreply.github.com>
2024-07-03 17:58:27 +0530

    From and import functionality

---
+++ FromAndImport.py
@@ -0,0 +1,22 @@
+#to use specific math function you have to use from keyword and have to define function names
+
+from math import sqrt,pi
+result= sqrt(9) *pi
+print(result)
+
+# importing math function you have t use import match
+
+# import math
+# result= match.sqrt(9)
+# print(result)
+
+
+#we can use (from math import *) to import all the functions
+# from math import *
+# result= sqrt(9) *pi
+# print(result)
+
+#Dir functions
+# import math
+# print(dir(math))
+# print(math.nan,type(math.nan)) ----- printing type(float)
