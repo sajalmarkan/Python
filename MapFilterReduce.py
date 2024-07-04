7ab963df619a48e010920e2b3afbf2bcd6415fbc
sajalmarkan <127577621+sajalmarkan@users.noreply.github.com>
2024-07-04 12:32:30 +0530

    Map Filter and Reduce

---
+++ MapFilterReduce.py
@@ -0,0 +1,23 @@
+# MAPS
+def cube(x):
+    return x*x*x
+
+l=[1,2,3,4,5]
+# newl = []
+# for i in l:
+#     newl.append(cube(i))
+newlist=list(map(cube,l))
+print(newlist)
+
+# Filter
+def filter_function(a):
+   return a>4
+newl = list(filter(filter_function,l))
+print(newl)
+
+# Reduce Function
+from functools import reduce
+def mysum(x,y):
+    return x+y
+sum = reduce(mysum,l)
+print(sum)
