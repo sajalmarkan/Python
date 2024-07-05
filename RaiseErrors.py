b3378981e4e064e083783e5d1fbe585dbe942485
sajalmarkan <127577621+sajalmarkan@users.noreply.github.com>
2024-07-05 16:55:49 +0530

    Raise Errors

--- RaiseErrors.py
+++ RaiseErrors.py
@@ -2,4 +2,4 @@ a= int(input("Enter the value between 5 and 9: "))
 if a<5 or a>9 :
     raise ValueError("value should be between 5 and 9")
 else:
-    print(f"The entered value is {a}")
+    print(f"The entered value is {a}") 
