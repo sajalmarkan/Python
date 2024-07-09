0dd532b33ee4169bddc7d3f9a18eb9547a5a898d
sajalmarkan <127577621+sajalmarkan@users.noreply.github.com>
2024-07-09 13:17:05 +0530

    Caching

---
+++ Caching.py
@@ -0,0 +1,20 @@
+from functools import lru_cache
+import time
+
+@lru_cache(maxsize=None)
+def fx(n):
+    time.sleep(5)
+    return n*5
+
+print(fx(20))
+print("Done for 20")
+print(fx(10))
+print("Done for 10")
+print(fx(5))
+print("Done for 5")
+print(fx(20))
+print("Done for 20")
+print(fx(10))
+print("Done for 10")
+print(fx(5))
+print("Done for 5")
